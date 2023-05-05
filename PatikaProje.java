4ee3d5c64b60220e18ccaad4cca10eeb5246254d
enes2424 <132240375+enes2424@users.noreply.github.com>
2023-05-05 12:24:00 +0300

    Update PatikaProje.java

--- PatikaProje.java
+++ PatikaProje.java
@@ -11,54 +11,55 @@ public class PatikaProje {
 		System.out.print("Doğduğunuz gün: ");
 		int gun = input.nextInt();
 		
-		if (ay == 1) {
+		if (ay == 1 && (gun >= 1 && gun <= 31)) {
 			if (gun <= 21) System.out.println("Oğlak Burcu");
 			else System.out.println("Kova Burcu");
 		}
-		else if (ay == 2) {
+		else if (ay == 2 && (gun >= 1 && gun <= 28)) {
 			if (gun <= 19) System.out.println("Kova Burcu");
 			else System.out.println("Balık Burcu");
 		}
-		else if (ay == 3) {
+		else if (ay == 3 && (gun >= 1 && gun <= 31)) {
 			if (gun <= 20) System.out.println("Balık Burcu");
 			else System.out.println("Koç Burcu");
 		}
-		else if (ay == 4) {
+		else if (ay == 4 && (gun >= 1 && gun <= 30)) {
 			if (gun <= 20) System.out.println("Koç Burcu");
 			else System.out.println("Boğa Burcu");
 		}
-		else if (ay == 5) {
+		else if (ay == 5 && (gun >= 1 && gun <= 31)) {
 			if (gun <= 21) System.out.println("Boğa Burcu");
 			else System.out.println("İkizler Burcu");
 		}
-		else if (ay == 6) {
+		else if (ay == 6 && (gun >= 1 && gun <= 30)) {
 			if (gun <= 22) System.out.println("İkizler Burcu");
 			else System.out.println("Yengeç Burcu");
 		}
-		else if (ay == 7) {
+		else if (ay == 7 && (gun >= 1 && gun <= 31)) {
 			if (gun <= 22) System.out.println("Yengeç Burcu");
 			else System.out.println("Aslan Burcu");
 		}
-		else if (ay == 8) {
+		else if (ay == 8 && (gun >= 1 && gun <= 31)) {
 			if (gun <= 22) System.out.println("Aslan Burcu");
 			else System.out.println("Başak Burcu");
 		}
-		else if (ay == 9) {
+		else if (ay == 9 && (gun >= 1 && gun <= 30)) {
 			if (gun <= 22) System.out.println("Başak Burcu");
 			else System.out.println("Terazi Burcu");
 		}
-		else if (ay == 10) {
+		else if (ay == 10 && (gun >= 1 && gun <= 31)) {
 			if (gun <= 22) System.out.println("Terazi Burcu");
 			else System.out.println("Akrep Burcu");
 		}
-		else if (ay == 11) {
+		else if (ay == 11 && (gun >= 1 && gun <= 30)) {
 			if (gun <= 21) System.out.println("Akrep Burcu");
 			else System.out.println("Yay Burcu");
 		}
-		else if (ay == 12) {
+		else if (ay == 12 && (gun >= 1 && gun <= 31)) {
 			if (gun <= 21) System.out.println("Yay Burcu");
 			else System.out.println("Oğlak Burcu");
 		}
+		else System.out.println("Error");
 	}
 
 }
